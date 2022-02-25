31c4064a274845ed14291d1a203d34800690d641
pirathapsiva <sspirathap26370@gmail.com>
2022-02-25 21:42:28 +0530

    development

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((req,res,next)=>{
     error.status = 404;
     next(error);
 })
-//when our custom error doesn't work
+
 app.use((error,req,res,next)=>{
     res.status (error.status||500);
     res.json({
